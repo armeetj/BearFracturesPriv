6de8a62f5f8135107f6295d533f0499f254dc4d1
xarmeetx <xarmeetx@gmail.com>
2019-11-01 17:44:40 -0700

    added validation data

--- engine/extract.py
+++ engine/extract.py
@@ -4,15 +4,29 @@ import csv
     
     
 imgnumb = 1;
-with open('engine/dataset/train_hand_paths.csv', newline='') as csvfile:
+with open('engine/train_hand_paths.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         print(row)
-        shutil.copy((row[0]+"image1.png"), "engine/XR_HAND_dataset/test/image"+ str(imgnumb) + ".png", follow_symlinks=True)
+        shutil.copy((row[0]+"image1.png"), "engine/XR_HAND_dataset/train/image"+ str(imgnumb) + ".png", follow_symlinks=True)
 
         if row[0]==0:
-            shutil.copy("engine/XR_HAND_dataset/result0.txt", "engine/XR_HAND_dataset/test/result"+ str(imgnumb) + ".txt", follow_symlinks=True)
+            shutil.copy("engine/XR_HAND_dataset/result0.txt", "engine/XR_HAND_dataset/train/result"+ str(imgnumb) + ".txt", follow_symlinks=True)
         else:
-            shutil.copy("engine/XR_HAND_dataset/result1.txt", "engine/XR_HAND_dataset/test/result"+ str(imgnumb) + ".txt", follow_symlinks=True)
+            shutil.copy("engine/XR_HAND_dataset/result1.txt", "engine/XR_HAND_dataset/train/result"+ str(imgnumb) + ".txt", follow_symlinks=True)
+       
+        imgnumb = imgnumb+1;
+
+imgnumb = 1;
+with open('engine/valid_hand_paths.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        print(row)
+        shutil.copy((row[0]+"image1.png"), "engine/XR_HAND_dataset/valid/image"+ str(imgnumb) + ".png", follow_symlinks=True)
+
+        if row[0]==0:
+            shutil.copy("engine/XR_HAND_dataset/result0.txt", "engine/XR_HAND_dataset/valid/result"+ str(imgnumb) + ".txt", follow_symlinks=True)
+        else:
+            shutil.copy("engine/XR_HAND_dataset/result1.txt", "engine/XR_HAND_dataset/valid/result"+ str(imgnumb) + ".txt", follow_symlinks=True)
        
         imgnumb = imgnumb+1;
