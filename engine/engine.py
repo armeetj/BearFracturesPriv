293d6005d2fc27bd5c743f3b40e89c1e3af95193
xarmeetx <xarmeetx@gmail.com>
2019-11-02 17:55:44 -0700

    temp commit

--- engine/engine.py
+++ engine/engine.py
@@ -12,6 +12,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+from matplotlib import image
+from matplotlib import pyplot
+
 #constants
 ROWS=400
 COLS=400
@@ -34,19 +37,56 @@ valid_images=[]
 
 #labels
 train_labels = []
-test_labels = []
+valid_labels = []
 
 #read in all training and validating images to raw arrays
 train_folder_raw_data = [i[2] for i in os.walk(TRAIN_PATH)]
 for folder in train_folder_raw_data:
-    print(folder)
     for f in folder:
-        print(f)
         if ".png" in f:
-            raw_train_images.append(f);
+            print(f)
+            train_images.append(image.imread(TRAIN_PATH + "/" + f));
+            img = Image.open(TRAIN_PATH + "/" + f)
+            print("read and converted png")
+            # create a thumbnail and preserve aspect ratio
+            img.thumbnail((ROWS,COLS))
+            print("resized image")
         else:
             with open (TRAIN_PATH + "/" + f, "r") as temp_label:
                 data=str(temp_label.readlines()[0])
                 train_labels.append(data);
+
+valid_folder_raw_data = [i[2] for i in os.walk(VALID_PATH)]
+for folder in valid_folder_raw_data:
+    for f in folder:
+        if ".png" in f:
+            print(f)
+            valid_images.append(image.imread(VALID_PATH + "/" + f));
+            img = Image.open(VALID_PATH + "/" + f)
+            print("read and converted png")
+            # create a thumbnail and preserve aspect ratio
+            img.thumbnail((ROWS,COLS))
+            print("resized image")
+        else:
+            with open (VALID_PATH + "/" + f, "r") as temp_label:
+                data=str(temp_label.readlines()[0])
+                valid_labels.append(data);
+
+print(train_images)
+print(valid_images)
+train_labels = np.array(train_labels)
+valid_labels = np.array(valid_labels)
+print(train_labels)
+print(valid_labels)
+
+""" print("train shape")
+print (train_images.shape)
+print("valid shape")
+print (valid_images.shape) """
+""" 
+# Normalize pixel values to be between 0 and 1
+train_images, valid_images = train_images / 255.0, valid_images / 255.0
+
+print(train_images) """
 
 print("Written by Armeet Singh Jatyani 2019")
