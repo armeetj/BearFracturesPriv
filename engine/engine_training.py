daccfe9b41edc26ef54538f217d92577137eddf9
xarmeetx <xarmeetx@gmail.com>
2019-11-06 14:31:44 -0800

    black

--- engine/engine_training.py
+++ engine/engine_training.py
@@ -7,7 +7,9 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from PIL import Image  
 
 import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -19,13 +21,16 @@ from matplotlib import pyplot
 ROWS=400
 COLS=400
 CHANNELS=1
-EPOCHS=10
+EPOCHS=5
+TRAIN_BATCH_SIZE = 8
+VALID_BATCH_SIZE = 8
 TRAIN_PATH = "engine/XR_HAND_dataset/small_train"
 VALID_PATH = "engine/XR_HAND_dataset/small_valid"
-
+#TRAIN_PATH = "engine/XR_HAND_dataset/train"
+#VALID_PATH = "engine/XR_HAND_dataset/valid"
 # Define the number of classes
 NUM_CLASSES = 2
-classes = {'normal' : 0 , 'broken' : 1}
+classes = {0 : "normal" , 1 : "broken"}
 
 #PNG files
 raw_train_images=[]
@@ -39,54 +44,93 @@ valid_images=[]
 train_labels = []
 valid_labels = []
 
-#read in all training and validating images to raw arrays
-train_folder_raw_data = [i[2] for i in os.walk(TRAIN_PATH)]
-for folder in train_folder_raw_data:
-    for f in folder:
-        if ".png" in f:
-            print(f)
-            train_images.append(image.imread(TRAIN_PATH + "/" + f));
-            img = Image.open(TRAIN_PATH + "/" + f)
-            print("read and converted png")
-            # create a thumbnail and preserve aspect ratio
-            img.thumbnail((ROWS,COLS))
-            print("resized image")
-        else:
-            with open (TRAIN_PATH + "/" + f, "r") as temp_label:
-                data=str(temp_label.readlines()[0])
-                train_labels.append(data);
+def read_data(images, labels, path):
+    folder_raw_data = [i[2] for i in os.walk(path)]
+    for folder in folder_raw_data:
+        for f in folder:
+            if ".png" in f:
+                images.append(image.imread(path + "/" + f));
+            else:
+                with open (path + "/" + f, "r") as temp_label:
+                    data=str(temp_label.readlines()[0])
+                    labels.append(data);
+def format_data():
+    train_image_generator = ImageDataGenerator(rescale=1./255)
+    valid_image_generator = ImageDataGenerator(rescale=1./255)
+    train_images = train_image_generator.flow_from_directory(batch_size=TRAIN_BATCH_SIZE,
+                                                           directory=TRAIN_PATH,
+                                                           shuffle=True,
+                                                           target_size=(ROWS, COLS),
+                                                           class_mode='binary')
+    valid_images = valid_image_generator.flow_from_directory(batch_size=VALID_BATCH_SIZE,
+                                                           directory=VALID_PATH,
+                                                           target_size=(ROWS, COLS),
+                                                           class_mode='binary')
 
-valid_folder_raw_data = [i[2] for i in os.walk(VALID_PATH)]
-for folder in valid_folder_raw_data:
-    for f in folder:
-        if ".png" in f:
-            print(f)
-            valid_images.append(image.imread(VALID_PATH + "/" + f));
-            img = Image.open(VALID_PATH + "/" + f)
-            print("read and converted png")
-            # create a thumbnail and preserve aspect ratio
-            img.thumbnail((ROWS,COLS))
-            print("resized image")
-        else:
-            with open (VALID_PATH + "/" + f, "r") as temp_label:
-                data=str(temp_label.readlines()[0])
-                valid_labels.append(data);
+def plot(z):
+    plt.figure(figsize=(3,3))
+    for i in range(z):
+        plt.subplot(int(sqrt(z))+1,int(sqrt(z))+1,i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(train_images[i])
+        plt.xlabel(classes[int(train_labels[i])])
+    plt.show()
 
-print(train_images)
-print(valid_images)
-train_labels_new = np.array(train_labels)
-valid_labels_new = np.array(valid_labels)
-print(train_labels)
-print(valid_labels)
+# read_data(train_images, train_labels, TRAIN_PATH)
+# read_data(valid_images, valid_labels, VALID_PATH)   
+format_data();       
 
-""" print("train shape")
-print (train_images.shape)
-print("valid shape")
-print (valid_images.shape) """
-""" 
-# Normalize pixel values to be between 0 and 1
-train_images, valid_images = train_images / 255.0, valid_images / 255.0
+model = Sequential([
+    Conv2D(16, 3, padding='same', activation='relu', input_shape=(ROWS, COLS ,3)),
+    MaxPooling2D(),
+    Conv2D(32, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    Conv2D(64, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    Flatten(),
+    Dense(512, activation='relu'),
+    Dense(1, activation='sigmoid')
+])
 
-print(train_images) """
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+model.summary()
 
-print("Written by Armeet Singh Jatyani 2019")
+history = model.fit_generator(
+    train_images,
+    steps_per_epoch=TRAIN_BATCH_SIZE // TRAIN_BATCH_SIZE,
+    epochs=EPOCHS,
+    validation_data=valid_images,
+    validation_steps=VALID_BATCH_SIZE // VALID_BATCH_SIZE
+)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(EPOCHS)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+valid_loss, valid_acc = model.evaluate(valid_images,  valid_labels, verbose=2)
+print("valid_acc: " + str(valid_acc))
+
+
+print("\nWritten by Armeet Singh Jatyani 2019")
